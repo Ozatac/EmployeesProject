a4bb97f77007cbaf559ecf44eb3a71b6545359bb
Tunahan Özataç <tunahan.ozatac@gmail.com>
2019-01-30 02:01:16 +0300

    proje devami

---
+++ src/DataBase.java
@@ -0,0 +1,11 @@
+public class DataBase {
+    
+    public static final String  kullanici_adi = "root"; // her yerden ulaşmak için
+    public static final String parola = "";
+    public static final String db_ismi = "demo";
+    public static final String host = "localhost";
+    public static final int port = 3306;
+    
+   
+    
+}
